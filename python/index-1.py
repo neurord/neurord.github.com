9a81c23b0c6f185c3ec04981973c69098be7c0f1
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2016-07-14 16:13:39 -0400

    Update python docs after HDF5 structure tweaks

--- python/index-1.py
+++ python/index-1.py
@@ -1,13 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from stochdiff import output
+
+from neurord import output
+
 out = output.Output('model.h5')
 counts = out.counts()
 gb = counts.groupby(level='voxel time  specie'.split())
+
 def stdp3(x):
    return np.mean(x) + np.std(x)*100
 def stdm3(x):
    return np.mean(x) - np.std(x)*100
+
 data = gb.aggregate([np.mean, stdp3, stdm3])
 data.plot()
 plt.show()
